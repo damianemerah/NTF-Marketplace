6d9815c4aa1bdd0ed238ee4457d630cccf73d89f
dammy995 <damianemerah1@gmail.com>
2025-02-18 13:18:12 +0100

    update default wallet

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-import { http, createConfig } from "wagmi";
+import { http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { getDefaultWallets, getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 const projectId = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
 
@@ -10,13 +10,20 @@ if (!projectId) {
   );
 }
 
+const { wallets } = getDefaultWallets({
+  appName: "NFT Marketplace",
+  projectId,
+  chains: [sepolia, mainnet],
+});
+
 // Wagmi Configuration
 export const config = getDefaultConfig({
   appName: "NFT Marketplace",
-  projectId: projectId,
+  projectId,
   chains: [sepolia, mainnet],
   transports: {
     [sepolia.id]: http(),
     [mainnet.id]: http(),
   },
+  wallets,
 });
